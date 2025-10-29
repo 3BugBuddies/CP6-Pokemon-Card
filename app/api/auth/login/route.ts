7208067c685b6c5533e8f505ea4aef43762f509d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-29 20:33:04 -0300

    add env

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 export async function POST(req: Request) {
   const { email, password } = await req.json()
 
-  if (email === "teste@email.com" && password === "123456") {
+  if (email === "teste@email.com" && password === process.env.USER_SECRET_PASSWORD) {
     const res = NextResponse.json({ success: true })
     res.cookies.set("user_session", "active", { httpOnly: true, path: "/" })
     return res
